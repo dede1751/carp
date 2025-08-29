d6f548b1cd1417da395bc1ccaa8e29cf7d90cbac
dede1751 <63297263+dede1751@users.noreply.github.com>
2025-08-29 20:51:11 +0200

    Unrolled SIMD for hidden->out Bench: 8416299

--- engine/src/nnue/inference.rs
+++ engine/src/nnue/inference.rs
@@ -5,6 +5,7 @@ use super::network::{CR_MAX, CR_MIN, HIDDEN, MODEL, QA, QAB, SCALE};
 use crate::search_params::Eval;
 use chess::piece::Color;
 
+#[cfg(simd_none)]
 mod scalar_eval {
     use super::*;
 
@@ -78,6 +79,92 @@ mod scalar_eval {
             for (&value, &weight) in them.zip(&MODEL.output_weights[HIDDEN..]) {
                 out += lizard_screlu(value, weight);
             }
+
+            ((out / QA + MODEL.output_bias as i32) * SCALE / QAB) as Eval
+        }
+    }
+}
+
+#[cfg(not(simd_none))]
+mod simd_eval {
+    use super::*;
+    use crate::nnue::simd::{self, ACC, VEC_I16_SIZE};
+
+    impl Accumulator {
+        pub fn update_weights<const ON: bool>(&mut self, idx: (usize, usize)) {
+            fn update<const ON: bool>(acc: &mut SideAccumulator, idx: usize) {
+                let zip = acc
+                    .iter_mut()
+                    .zip(&MODEL.feature_weights[idx..idx + HIDDEN]);
+
+                for (acc_val, &weight) in zip {
+                    if ON {
+                        *acc_val += weight;
+                    } else {
+                        *acc_val -= weight;
+                    }
+                }
+            }
+
+            update::<ON>(&mut self.white, idx.0);
+            update::<ON>(&mut self.black, idx.1);
+        }
+
+        pub fn add_sub_weights(&mut self, from: (usize, usize), to: (usize, usize)) {
+            fn add_sub(acc: &mut SideAccumulator, from: usize, to: usize) {
+                let zip = acc.iter_mut().zip(
+                    MODEL.feature_weights[from..from + HIDDEN]
+                        .iter()
+                        .zip(&MODEL.feature_weights[to..to + HIDDEN]),
+                );
+
+                for (acc_val, (&remove_weight, &add_weight)) in zip {
+                    *acc_val += add_weight - remove_weight;
+                }
+            }
+
+            add_sub(&mut self.white, from.0, to.0);
+            add_sub(&mut self.black, from.1, to.1);
+        }
+    }
+
+    impl NNUEState {
+        pub fn evaluate(&self, side: Color) -> Eval {
+            let acc = &self.accumulator_stack[self.current_acc];
+            let (us, them) = match side {
+                Color::White => (&acc.white, &acc.black),
+                Color::Black => (&acc.black, &acc.white),
+            };
+
+            let out = unsafe {                
+                let cr_min = simd::set_i16(CR_MIN);
+                let cr_max = simd::set_i16(CR_MAX);
+                let mut sum1 = simd::zero_i32::<ACC>();
+                let mut sum2 = simd::zero_i32::<ACC>();
+
+                let x1_ptr = us.as_ptr();
+                let x2_ptr = them.as_ptr();
+                let w1_ptr = MODEL.output_weights.as_ptr();
+                let w2_ptr = w1_ptr.add(HIDDEN);
+
+                for i in (0..HIDDEN).step_by(VEC_I16_SIZE * ACC) {
+                    let x1 = simd::load_i16::<ACC>(x1_ptr.add(i));
+                    let x2 = simd::load_i16::<ACC>(x2_ptr.add(i));
+                    let w1 = simd::load_i16::<ACC>(w1_ptr.add(i));
+                    let w2 = simd::load_i16::<ACC>(w2_ptr.add(i));
+
+                    let v1 = simd::clamp_i16(x1, cr_min, cr_max);
+                    let v2 = simd::clamp_i16(x2, cr_min, cr_max);
+
+                    let vw1 = simd::mullo_i16(v1, w1);
+                    let vw2 = simd::mullo_i16(v2, w2);
+
+                    sum1 = simd::fmadd_i16(sum1, v1, vw1);
+                    sum2 = simd::fmadd_i16(sum2, v2, vw2);
+                }
+
+                simd::hsum_i32(simd::sum_reduce_i32(simd::add_i32(sum1, sum2)))
+            };
 
             ((out / QA + MODEL.output_bias as i32) * SCALE / QAB) as Eval
         }
--- engine/src/nnue/simd/avx512.rs
+++ engine/src/nnue/simd/avx512.rs
@@ -1,1 +1,71 @@
+#![allow(non_camel_case_types)]
+use std::arch::avx512f::*;
+use std::array;
+use std::mem::size_of;
+use super::mm_shuffle;
 
+pub type VecI16 = __m512i;
+pub type VecI32 = __m512i;
+
+pub const VEC_I16_SIZE: usize = size_of::<VecI16>() / size_of::<i16>();
+pub const ACC: usize = 2; // number of accumuators to use for unrolling
+
+//////////////////////////////// i16 ////////////////////////////////
+#[inline(always)]
+pub unsafe fn set_i16(val: i16) -> VecI16 {
+    _mm512_set1_epi16(val)
+}
+#[inline(always)]
+pub unsafe fn load_i16<const N: usize>(ptr: *const i16) -> [VecI16; N] {
+    array::from_fn(|i| _mm512_load_si512(ptr.add(i * VEC_I16_SIZE)))
+}
+#[inline(always)]
+pub unsafe fn clamp_i16<const N: usize>(val: [VecI16; N], min: VecI16, max: VecI16) -> [VecI16; N] {
+    array::from_fn(|i| _mm512_min_epi16(max, _mm512_max_epi16(val[i], min)))
+}
+#[inline(always)]
+pub unsafe fn mullo_i16<const N: usize>(a: [VecI16; N], b: [VecI16; N]) -> [VecI16; N] {
+    array::from_fn(|i| _mm512_mullo_epi16(a[i], b[i]))
+}
+#[cfg(simd_avx512vnni)]
+#[inline(always)]
+pub unsafe fn fmadd_i16<const N: usize>(
+    sum: [VecI32; N],
+    a: [VecI16; N],
+    b: [VecI16; N],
+) -> [VecI32; N] {
+    array::from_fn(|i| _mm512_dpwssd_epi32(sum[i], a[i], b[i]))
+}
+#[cfg(simd_avx512f)]
+#[inline(always)]
+pub unsafe fn fmadd_i16<const N: usize>(
+    sum: [VecI32; N],
+    a: [VecI16; N],
+    b: [VecI16; N],
+) -> [VecI32; N] {
+    let prod_32 = array::from_fn(|i| _mm512_madd_epi16(a[i], b[i]));
+    array::from_fn(|i| _mm512_add_epi32(sum[i], prod_32[i]))
+}
+
+//////////////////////////////// i32 ////////////////////////////////
+#[inline(always)]
+pub unsafe fn zero_i32<const N: usize>() -> [VecI32; N] {
+    [_mm512_setzero_epi32(); N]
+}
+#[inline(always)]
+pub unsafe fn add_i32<const N: usize>(a: [VecI32; N], b: [VecI32; N]) -> [VecI32; N] {
+    array::from_fn(|i| _mm512_add_epi32(a[i], b[i]))
+}
+#[inline(always)]
+pub unsafe fn sum_reduce_i32<const N: usize>(val: [VecI32; N]) -> VecI32 {
+    let mut sum = val[0];
+    for v in val.iter().take(N).skip(1) {
+        sum = _mm512_add_epi32(sum, *v);
+    }
+    sum
+}
+// See: https://stackoverflow.com/a/60109639
+#[inline(always)]
+pub unsafe fn hsum_i32(val: VecI32) -> i32 {
+    _mm512_reduce_add_epi32(val)
+}
